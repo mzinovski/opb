3ead12b2545dcd9377202bbe4d21ba3cf899278e
Marijan <mzinovski@gmail.com>
2024-01-09 01:57:53 +0100

    color

--- resources/views/landing/partials/header.blade.php
+++ resources/views/landing/partials/header.blade.php
@@ -5,7 +5,7 @@
     <nav class="navbar navbar-expand-lg  py-4" id="navbar">
         <div class="container">
           <a class="navbar-brand" href="index.html">
-            Mega<span>kit.</span>
+            <img src="/imgs/OPBlogo.webp" style="width: 15%;">
           </a>
 
           <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarsExample09" aria-controls="navbarsExample09" aria-expanded="false" aria-label="Toggle navigation">
@@ -15,10 +15,10 @@
           <div class="collapse navbar-collapse text-center" id="navbarsExample09">
             <ul class="navbar-nav ml-auto">
               <li class="nav-item active">
-                <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-link" href="index.html">Почетна</a>
               </li>
               <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">About</a>
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdown03" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">За нас</a>
                     <ul class="dropdown-menu" aria-labelledby="dropdown03">
                         <li><a class="dropdown-item" href="about.html">Our company</a></li>
                         <li><a class="dropdown-item" href="pricing.html">Pricing</a></li>
@@ -35,11 +35,11 @@
                         <li><a class="dropdown-item" href="blog-single.html">Blog Single</a></li>
                     </ul>
               </li>
-               <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+               
             </ul>
 
             <form class="form-lg-inline my-2 my-md-0 ml-lg-4 text-center">
-              <a href="contact.html" class="btn btn-solid-border btn-round-full">Get a Quote</a>
+              <a href="contact.html" class="btn btn-solid-border btn-round-full">Контакт</a>
             </form>
           </div>
         </div>
